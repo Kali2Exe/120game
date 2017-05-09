92f19bd2fe6989ae1b01b4436f1cd98815a30e77
Seth <sacrawfo@ucsc.edu>
2017-05-09 16:26:17 -0700

    Enemy.js

--- js/Enemy.js
+++ js/Enemy.js
@@ -17,14 +17,27 @@ function Enemy (game, frame, x, y) {
     //enable physics and set to random velocity
     game.physics.arcade.enable(this);
     this.body.collideWorldBounds = true;
+	
     //this.body.velocity.x = game.rnd.integerInRange(0, 400);
 
 }
 //constructor
 Enemy.prototype = Object.create(Phaser.Sprite.prototype);
 Enemy.prototype.constructor = Enemy;
-
+var xspeed = 2;
+var yspeed = 2;
 Enemy.prototype.update = function() {
+	
+	this.x += xspeed;
+	if(this.x >= game.world.width-80 || this.x <= 80){ //80 = half of sprite width
+		xspeed = -xspeed;
+	}
+	this.y += yspeed;
+	if(this.y >= game.world.height-80 || this.y <= 80){//80 = half of sprite height
+		yspeed = -yspeed;
+	}
+	
+	
 
 
     //wraps around world
